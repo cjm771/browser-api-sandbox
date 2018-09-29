be6f5b2a50d2ee72fc6ff62ae0bb537d93ef89f2
Chris Malcolm <chrisishere@gmail.com>
2018-09-28 23:54:55 -0700

    [WIP] port fix?

--- examples/lightSabber/server.js
+++ examples/lightSabber/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use("/", express.static(__dirname + '/public'));
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('listening on port ' + port);
 });
